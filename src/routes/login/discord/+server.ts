eb17e5bc54fe532a87ac6ef6ff90c8c4f8cd5b24
Tom Prince <tom.prince@hocat.ca>
2023-03-13 17:29:18 -0600

    Debugging.

--- src/routes/login/discord/+server.ts
+++ src/routes/login/discord/+server.ts
@@ -59,17 +59,22 @@ class DiscordToken {
 }
 
 async function exchangeOAuthCode(code: string, redirect_uri: URL): Promise<DiscordToken> {
-    console.log({ body: new URLSearchParams({
-        client_id: publicEnv.PUBLIC_DISCORD_CLIENT_ID,
-        client_secret: env.VITE_DISCORD_CLIENT_SECRET,
-        grant_type: "authorization_code",
-        code: code,
-        redirect_uri: redirect_uri.toString(),
-    })})
+	console.log({
+		body: new URLSearchParams({
+			client_id: publicEnv.PUBLIC_DISCORD_CLIENT_ID,
+			client_secret: env.VITE_DISCORD_CLIENT_SECRET,
+			grant_type: "authorization_code",
+			code: code,
+			redirect_uri: redirect_uri.toString(),
+		}),
+	});
 	const response: RESTPostOAuth2AccessTokenResult = await makeRequest(
 		Routes.oauth2TokenExchange(),
 		{
 			method: "POST",
+			headers: {
+				"Content-Type": "application/x-www-form-urlencoded",
+			},
 			body: new URLSearchParams({
 				client_id: publicEnv.PUBLIC_DISCORD_CLIENT_ID,
 				client_secret: env.VITE_DISCORD_CLIENT_SECRET,
