66fcfeaa05959f996262ab351e8413a7a7a52a76
Tom Prince <tom.prince@hocat.ca>
2023-03-26 14:10:03 -0600

    cal-bot: Idempotency.
    - Add calendar before trying to add events to it.
    - Try updating then adding events to calendar.
    - Handle non-json errors from google.

--- packages/cal-bot/src/google.ts
+++ packages/cal-bot/src/google.ts
@@ -7,9 +7,15 @@ export async function makeRequest<ResponseType>(
 ): Promise<ResponseType> {
 	const response = await fetch(route, init);
 	if (!response.ok) {
-		const json = await response.json();
-		console.log(json);
-		throw json;
+		const text = await response.text();
+		try {
+			const json = JSON.parse(text);
+			console.log("Error making request to google:", json);
+			throw json;
+		} catch {
+			console.log("Error making request to google:", text);
+			throw text;
+		}
 	}
 	return response.json();
 }
@@ -42,6 +48,21 @@ export async function getServiceToken(
 	return new GoogleToken({ access_token, token_type: "Bearer" });
 }
 
+export async function insertEvent(
+	googleToken: GoogleToken,
+	calendarId: string,
+	event: calendar_v3.Schema$Event,
+): Promise<calendar_v3.Schema$Event> {
+	return makeRequest(`https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events/`, {
+		headers: {
+			...googleToken.getHeaders(),
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(event),
+		method: "POST",
+	});
+}
+
 export async function patchEvent(
 	googleToken: GoogleToken,
 	calendarId: string,
@@ -59,4 +80,18 @@ export async function patchEvent(
 			method: "PATCH",
 		},
 	);
+}
+
+export async function insertCalendarList(
+	googleToken: GoogleToken,
+	calendarId: string,
+): Promise<calendar_v3.Schema$CalendarListEntry> {
+	return makeRequest(`https://www.googleapis.com/calendar/v3/users/me/calendarList/`, {
+		headers: {
+			...googleToken.getHeaders(),
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({ id: calendarId }),
+		method: "POST",
+	});
 }
